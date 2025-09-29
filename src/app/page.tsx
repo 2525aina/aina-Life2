d984cff7507c1b45d64d29a0656e262d046cb9ca
2525aina <dai.0525n@gmail.com>
2025-09-29 09:13:54 +0900

    Revert "fix: ホーム画面のタスクボタンと日付ナビゲーションがスクロールしないように固定"
    This reverts commit a9395ee7cf791aed04e9ac9bd677cf05c841dfce.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-            <div className="md:col-span-1 space-y-4 sticky top-0 z-10 bg-background"> {/* Added sticky, top-0, z-10, bg-background */}
+            <div className="md:col-span-1 space-y-4">
               <TaskSelector />
             </div>
             <div className="md:col-span-2">
