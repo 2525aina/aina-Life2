5641b91b566025395cb272d85cb0f0735f656091
2525aina <dai.0525n@gmail.com>
2025-09-29 20:54:50 +0900

    feat: トースト表示位置を上部中央に変更

--- src/components/ToastProvider.tsx
+++ src/components/ToastProvider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export function ToastProvider() {
   const { userProfile } = useUser();
-  const toastPosition = userProfile?.settings?.toastPosition || "bottom-right";
+  const toastPosition = userProfile?.settings?.toastPosition || "top-center";
 
   return <Toaster position={toastPosition} richColors offset="64px" />;
 }
