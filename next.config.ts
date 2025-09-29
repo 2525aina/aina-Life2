e875bb1a0a6d9a9ec2589b88258ee23f5cf2ba7c
2525aina <dai.0525n@gmail.com>
2025-09-29 13:41:55 +0900

    fix(image): next/image設定と構文エラーを修正
    next/imageコンポーネントがlocalhost:9199からの画像を読み込めるようにnext.config.tsを設定しました。また、設定時の構文エラーも修正しました。

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9199',
+        pathname: '/v0/b/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
